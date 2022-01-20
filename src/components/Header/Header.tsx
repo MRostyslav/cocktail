2a4f275585a418858f4a7e95238f3c5d85a3552f
MRostyslav <rostyslav.mykhalskyy@gmail.com>
2022-01-20 15:08:31 +0200

    feat: project structure

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import styles from './Header.module.scss';
+
+function Header() {
+    return <header className="App-header">
+
+    </header>;
+}
+
+export default Header;
