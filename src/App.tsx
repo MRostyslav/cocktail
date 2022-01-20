079e458e990f77e80073be48b3ffe271c5d5d804
MRostyslav <rostyslav.mykhalskyy@gmail.com>
2022-01-20 16:02:09 +0200

    feat: header and logo

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,24 @@
 import React from 'react';
 import { Counter } from './features/counter/Counter';
 import './App.scss';
+import Header from './components/Header/Header';
+import { Route, Routes } from 'react-router-dom';
+
+// Routes
+import Home from './views/Home/Home';
+import MyCocktails from './views/MyCocktails/MyCocktails';
+import Search from './views/Search/Search';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <Counter />
-      </header>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/my-cocktails" element={<MyCocktails />} />
+        <Route path="/search" element={<Search />} />
+      </Routes>
     </div>
   );
 }
