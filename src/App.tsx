86bbf52119b6d7ca63f90fcd4612a4a94c65eecb
MRostyslav <rostyslav.mykhalskyy@gmail.com>
2022-01-21 11:03:52 +0200

    feat: home page

--- src/App.tsx
+++ src/App.tsx
@@ -14,11 +14,14 @@ function App() {
   return (
     <div className="App">
       <Header />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/my-cocktails" element={<MyCocktails />} />
-        <Route path="/search" element={<Search />} />
-      </Routes>
+      <div className="App__layoute">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/my-cocktails" element={<MyCocktails />} />
+          <Route path="/search" element={<Search />} />
+        </Routes>
+      </div>
+
     </div>
   );
 }
--- src/views/Home/Home.tsx
+++ src/views/Home/Home.tsx
@@ -1,5 +1,25 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import styles from './Home.module.scss';
+
+import { PrimaryBtn } from '../../components/Buttons/';
+import Cocktail from '../../features/cocktail/Cocktail'
 
 export default function Home() {
-    return <div>Home</div>;
+    const navigate = useNavigate();
+
+    const goToSearch = (): void => navigate('/search');
+
+    return <div className={styles.home}>
+        <h2 className={styles.title}>
+            Cocktail helper
+        </h2>
+        <p className={styles.description}>This awsame project can help you create amazing cocktails!</p>
+        <div className={styles.details}>
+            <p className={styles.details__text}>You can click 'search' and find somthing what you like</p>
+            <PrimaryBtn text={'Search'} action={goToSearch}></PrimaryBtn>
+            <p className={styles.details__text}>or try this cocktail</p>
+            <Cocktail></Cocktail>
+        </div>
+    </div>;
 }
